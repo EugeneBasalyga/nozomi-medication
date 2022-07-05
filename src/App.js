c5f1c29bcf8467d664bb3d9dc5221d737557fc96
Yauheni Basalyha <basalygaeugene@gmail.com>
2022-07-05 13:05:23 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
-import axios from 'axios';
 
 import './components/LoginForm/LoginForm';
 import { AuthProvider } from './contexts/auth';
 import AppRouter from './router/AppRouter';
-
-axios.defaults.baseURL = 'http://localhost:3001';
 
 const App = () => {
   return (
